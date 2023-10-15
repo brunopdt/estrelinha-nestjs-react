63b1e00357e6d526be0c0bb4e2a97277bcff131d
brunopdt <bruno.pduarte0@gmail.com>
2023-10-15 19:53:10 -0300

    refactor login method

--- implementacao/api/src/modules/login/login.controller.ts
+++ implementacao/api/src/modules/login/login.controller.ts
@@ -1,21 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDTO } from './dto/Login.dto';
 import { LoginService } from './login.service';
+import { Aluno, Empresa } from '@prisma/client';
 
 @Controller('login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
   @Post()
-  async login(@Body() loginDTO: LoginDTO):  Promise<{
-    data: {
-        Aluno;
-        Empresa;
-    };
-}>{
+  async login(@Body() loginDTO: LoginDTO):  Promise<Empresa | Aluno>{
     const res = await this.loginService.login(loginDTO);
 
-    return {data: res}
+    return res?.Aluno ?? res?.Empresa
   }
 
 }
